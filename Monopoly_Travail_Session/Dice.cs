8a6cf2490b923eee3764e583adc9a56b6392df63
martinPelletier <pelletiermartin@live.ca>
2017-12-09 23:18:49 -0500

    Mise à jour Dice

--- Monopoly_Travail_Session/Dice.cs
+++ Monopoly_Travail_Session/Dice.cs
@@ -7,8 +7,38 @@ namespace Monopoly
 {
     public class Dice
     {
-        public int[]  Brasser()
+        protected int deUn;
+        protected int deDeux;
+        
+        public Dice(int deUn, int deDeux)
         {
+            this.deUn = deUn;
+            this.deDeux = deDeux;
+        }
+
+        public int Brasser()
+        {
+            Random lance1 = new Random();
+            deUn = lance1.Next(1, 7);
+            Console.WriteLine(deUn);
+            Random lance2 = new Random();
+            deDeux = lance2.Next(1, 7);
+            Console.WriteLine(deDeux);
+            int totalDe = deUn + deDeux;
+            Console.WriteLine(totalDe);
+            return totalDe;
+        }
+        public bool VerifDouble()
+        {
+            bool doubleDe = false;
+            if (deUn == deDeux)
+            {
+                doubleDe = true;
+                Console.WriteLine(doubleDe);
+            }
+            return doubleDe;
+
+            /*
             int[] resultat = new int[2];
             Random aleatoire = new Random();
             //resultat[0] = aleatoire.Next(1, 7);
@@ -16,6 +46,7 @@ namespace Monopoly
             resultat[0] = 2;
             resultat[1] = 1;
             return resultat;
+            */
         }
     }
 }
