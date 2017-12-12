9475030f745dab0c624bd81c4e131e206515e175
Jeanne <Jeanne@MAGIFIQUE>
2017-12-12 10:34:53 -0500

    Gare

--- Monopoly_Travail_Session/Gare.cs
+++ Monopoly_Travail_Session/Gare.cs
@@ -25,27 +25,26 @@ namespace Monopoly
         //Méthode
         public override void Action(Joueur a)
         {
-
-            /*if(hypothequer = false)
-             Payer(a);*/
+        if(hypothequer = false)
+             Payer(a);
         }
 
         //Méthode
-       /* public void Acheter(Joueur a)
+        public void Acheter(Joueur a)
         {
-           if(a.argent >= prix)
+           if(a.Argent >= prix)
            {
              Console.WriteLine("Voulez-vous acheter "+nom+" pour "+prix+"$?\n O/N");
              if(Console.ReadLine().Equals("O")||Console.ReadLine().Equals("o"))
               { 
-                a.argent = a.argent - prix;
-                proprietaire = a.nom;
-                Console.WriteLine("Félicitation, vous êtes le nouveau propriétaire de "+nom+"! Il vous reste "+a.argent+"$.");
+                a.Argent = a.Argent - prix;
+                proprietaire = a.Identifiant;
+                Console.WriteLine("Félicitation, vous êtes le nouveau propriétaire de "+nom+"! Il vous reste "+a.Argent+"$.");
 
               }
             else
                 {
-               Console.WriteLine(+nom+ "est mis au Enchère!");
+                  Console.WriteLine(+nom+ "est mis au Enchère!");
                 }
              Console.ReadKey();
              Console.Clear();
@@ -53,13 +52,13 @@ namespace Monopoly
         }
         public void Hypothequer(Joueur a)
         {
-           if(locataire.Equals(a.nom))
+           if(locataire.Equals(a.Identifiant))
                 Console.WriteLine("Voulez-vous hypothèquer "+nom+" pour "+hypotheque+"$?\n O/N");
              if(Console.ReadLine().Equals("O")||Console.ReadLine().Equals("o"))
               { 
-                a.argent = a.argent + hypotheque;
+                a.Argent = a.Argent + hypotheque;
                 hypothequer = true;
-                Console.WriteLine("Vous avec hypothèquer "+nom+"! Vous avez maintenant "+a.argent+"$.");
+                Console.WriteLine("Vous avec hypothèquer "+nom+"! Vous avez maintenant "+a.Argent+"$.");
 
               }
             else
@@ -71,7 +70,7 @@ namespace Monopoly
 
         public virtual void Payer(Joueur a)
         {
-            if(proprietaire.Equals(a.nom))
+            if(proprietaire.Equals(a.Identifiant))
                 Console.WriteLine(nom+" est à vous! Vous n'avez rien à payer!");
             else
             {
@@ -82,22 +81,23 @@ namespace Monopoly
                 else
                 {
                     Console.WriteLine("Vous devez payer "+prix+" à "+proprietaire+"!");
-                    if(a.argent>location)
-                        a.argent = a.argent - location;
-                    else
+                    if(a.Argent<location)
                     {
                         do
                         {
                             foreach(Case el in monopoly)
                             {
-                              if(el.proprietaire.Equals(a.nom))
+                              if(el.proprietaire.Equals(a.Identifiant))
                                 Hypothequer(a);
                             }
-                        }while(a.argent<location);
+                        }while(a.Argent<location);
                     }
-                
+                    a.Argent = a.Argent - location;
+
                 }
+                
+                
             }
-        }*/
+        }
     }
 }
