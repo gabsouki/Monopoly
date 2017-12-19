36741bade3647ebb0e315487266d4c9290b7ba54
gabsouki <soucy.gabriel@gmail.com>
2017-12-19 14:35:09 -0500

    Random choice

--- Monopoly_Travail_Session/Joueurs.cs
+++ Monopoly_Travail_Session/Joueurs.cs
@@ -8,11 +8,13 @@ namespace Monopoly
     public class Joueurs
     {
         public static List<Joueur> joueurs;
+        private List<Joueur> originale;
         protected int faillites;
 
         public Joueurs()
         {
             joueurs = new List<Joueur>();
+            originale = new List<Joueur>();
             faillites = 0;
         }
         public int Ajouter()
@@ -24,8 +26,25 @@ namespace Monopoly
                 Console.WriteLine("\nEntrez le nom du joueur {0}", i);
                 string nom = Console.ReadLine();
                 joueurs.Add(new Joueur(i, nom));
+                originale.Add(new Joueur(i, nom));
             }
             Console.WriteLine("\n{0} Joueurs ont étés ajoutés.", nombre);
+            Random rnd = new Random();
+            int random = 2;//rnd.Next(0, joueurs.Count-1);
+            if (random != 0)
+            {
+                for (int i = random; i < joueurs.Count; i++)
+                {
+                    joueurs[i - random] = joueurs[i];
+                }
+                int j = 0;
+                for (int i = joueurs.Count - random; i < joueurs.Count; i++)
+                {
+                    joueurs[i] = originale[j];
+                    j++;
+                }
+            }
+            Console.WriteLine("Le premier joueur a été choisis au hazard, c'est {0} qui commence.", joueurs[0].Nom);
             Console.WriteLine("\nAppuyez sur une touche pour commencer la partie.");
             Console.ReadKey();
             return nombre;
